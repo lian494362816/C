a2ed45613b6b5fe73d2b0d01da22d96b21feb8df
black.lian <liancancai001@163.com>
2018-03-11 22:30:18 +0800

    Add 17_Chapter/tree

---
+++ BOOKS/POINTERS_ON_C/17_Chapter/tree/static_tree/my_queue.h
@@ -0,0 +1,15 @@
+#ifndef __MY_QUEUE_H_
+
+#include "my_type.h"
+
+#define QUEUE_TYPE  int
+
+S32 my_queue_insert(QUEUE_TYPE Value);
+S32 my_queue_delete(void);
+QUEUE_TYPE my_queue_first(void);
+
+S32 my_queue_create(void);
+S32 my_queue_destroy(void);
+S32 my_queue_test(void);
+
+#endif //__MY_QUEUE_H_
