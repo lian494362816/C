a2eb18d299bf75583c4bc8b359d63d245c6f2cfb
black.lian <liancancai001@163.com>
2018-09-02 15:52:25 +0800

    Add 012 relocate Update README.txt

---
+++ 2440/012_relocate/007/src/.init.o.d
@@ -0,0 +1,1 @@
+init.o: init.c ../include/s3c2440_soc.h ../include/init.h
---
+++ 2440/012_relocate/007/src/.main.o.d
@@ -0,0 +1,2 @@
+main.o: main.c ../include/uart.h ../include/delay.h \
+  ../include/my_printf.h ../include/uart.h ../include/init.h
