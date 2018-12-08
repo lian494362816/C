089877502656d9327cde5c67a9a700c6b4846aa8
black.lian <liancancai001@163.com>
2018-12-09 02:58:17 +0800

    Update 018_cache_mmu Makefile

---
+++ 2440/018_cache_mmu/002_enbale_mmu/module/.main.o.d
@@ -0,0 +1,15 @@
+main.o: main.c \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/uart.h \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/key.h \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/led.h \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/delay.h \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/my_printf.h \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/uart.h \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/init.h \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/interrupt.h \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/s3c2440_soc.h \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/my_error.h \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/common.h \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/my_printf.h \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/lcd.h \
+  /home/black/C/2440/018_cache_mmu/002_enbale_mmu/include/timer.h
